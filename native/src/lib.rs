52c7e26e69bd22ec83bea6a658a43822de4daae7
Andrej Rasevic <andrej@rasevicengineering.com>
2021-11-20 17:17:54 -0500

    initial piping in place

---
+++ native/src/lib.rs
@@ -0,0 +1,27 @@
+use electron_rust_terminal_emulator_backend::*;
+use neon::prelude::*;
+
+fn marshalling_example_add(mut cx: FunctionContext) -> JsResult<JsNumber> {
+ let a = cx.argument::<JsNumber>(0)?.value();
+ let b = cx.argument::<JsNumber>(1)?.value();
+ let r = electron_rust_terminal_emulator_backend::example_add(a as f32, b as f32);
+ Ok(cx.number(r))
+}
+
+fn marshalling_example_concat(mut cx: FunctionContext) -> JsResult<JsString> {
+ let a = cx.argument::<JsString>(0)?.value();
+ let b = cx.argument::<JsString>(1)?.value();
+ let r = example_concat(&a, &b);
+ Ok(cx.string(r))
+}
+
+fn marshalling_hello_from_rust(mut cx: FunctionContext) -> JsResult<JsString> {
+    let r = hello_from_rust();
+    Ok(cx.string(r))
+   }
+
+register_module!(mut cx, {
+ cx.export_function("example_add", marshalling_example_add)?;
+ cx.export_function("example_concat", marshalling_example_concat);
+ cx.export_function("hello_from_rust", marshalling_hello_from_rust)
+});
---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const p = window.preload;
+
+function App() {
+ return (
+  <div className="App">
+   <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>example output of execution of rust functions called within node</p>
+    <p>{p.native.hello_from_rust()}</p>
+    <ul>
+     <li>p.is_dev = {p.is_dev.toString()}</li>
+     <li>
+      p.native.example_add( Math.PI, 1.23 ) ={" "}
+      {p.native.example_add(Math.PI, 1.23)}
+     </li>
+     <li>
+      p.native.example_add( -1.23, Math.E ) ={" "}
+      {p.native.example_add(-1.23, Math.E)}
+     </li>
+     <li>
+      p.native.example_concat( "🍣su", "shi🍣" ) ={" "}
+      {p.native.example_concat("🍣su", "shi🍣")}
+     </li>
+     <li>
+      p.native.example_concat( "🌶haba", "nero🌶" ) ={" "}
+      {p.native.example_concat("🌶haba", "nero🌶")}
+     </li>
+    </ul>
+   </header>
+  </div>
+ );
+}
+
+export default App;
