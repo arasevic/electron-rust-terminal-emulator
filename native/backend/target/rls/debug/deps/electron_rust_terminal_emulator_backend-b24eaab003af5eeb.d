4088ccfb87497566c9a7aa9a2f974b3ab98886dc
Andrej Rasevic <andrej@rasevicengineering.com>
2021-12-13 11:30:11 -0500

    clean up

--- native/backend/target/rls/debug/deps/electron_rust_terminal_emulator_backend-b24eaab003af5eeb.d
+++
@@ -1,5 +0,0 @@
-/Users/ybar/git/cmsc388Zfall21/electron-rust-terminal-emulator/native/backend/target/rls/debug/deps/electron_rust_terminal_emulator_backend-b24eaab003af5eeb.rmeta: native/backend/src/lib.rs
-
-/Users/ybar/git/cmsc388Zfall21/electron-rust-terminal-emulator/native/backend/target/rls/debug/deps/electron_rust_terminal_emulator_backend-b24eaab003af5eeb.d: native/backend/src/lib.rs
-
-native/backend/src/lib.rs:
