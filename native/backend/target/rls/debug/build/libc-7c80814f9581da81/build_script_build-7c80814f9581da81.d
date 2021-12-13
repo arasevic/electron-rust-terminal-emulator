34de555e7c8960aa2582544032af5971a29569c0
Andrej Rasevic <andrej@rasevicengineering.com>
2021-12-13 11:01:25 -0500

    more cleanup

--- native/backend/target/rls/debug/build/libc-7c80814f9581da81/build_script_build-7c80814f9581da81.d
+++
@@ -1,5 +0,0 @@
-/Users/ybar/git/cmsc388Zfall21/electron-rust-terminal-emulator/native/backend/target/rls/debug/build/libc-7c80814f9581da81/build_script_build-7c80814f9581da81: /Users/ybar/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.109/build.rs
-
-/Users/ybar/git/cmsc388Zfall21/electron-rust-terminal-emulator/native/backend/target/rls/debug/build/libc-7c80814f9581da81/build_script_build-7c80814f9581da81.d: /Users/ybar/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.109/build.rs
-
-/Users/ybar/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.109/build.rs:
